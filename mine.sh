f09746a48b30d04b61802341e67cd6ce037c8936
sci4me <minecraftman7778@gmail.com>
2015-03-09 14:44:14 -0500

    time for a fresh start

--- mine.sh
+++
@@ -1,1 +0,0 @@
-java -server -Xmx4G -Xms4G -XX:NewSize=3584m -XX:-UseConcMarkSweepGC -XX:+AggressiveOpts -XX:+OptimizeStringConcat -jar out/artifacts/SKristMiner_jar/SKristMiner.jar -threads 8 -nonces 30000000 k640c183io
--- src/main/java/com/sci/skristminer/mine/MinerListener.java
+++
@@ -1,9 +0,0 @@
-package com.sci.skristminer.mine;
-
-/**
- * @author sci4me
- */
-public interface MinerListener
-{
-    void onFinish(final Miner miner);
-}
