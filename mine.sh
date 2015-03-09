8037517072e6c7263bbfce872d5c3b279e3e6e13
sci4me <minecraftman7778@gmail.com>
2015-03-09 05:49:27 -0500

    nar

--- mine.sh
+++ mine.sh
@@ -1,1 +1,1 @@
-java -server -Xmx4G -Xms4G -XX:NewSize=3584m -XX:-UseConcMarkSweepGC -XX:+AggressiveOpts -jar out/artifacts/SKristMiner_jar/SKristMiner.jar -threads 8 -nonces 30000000 k640c183io
+java -server -Xmx4G -Xms4G -XX:NewSize=3584m -XX:-UseConcMarkSweepGC -XX:+AggressiveOpts -XX:+OptimizeStringConcat -jar out/artifacts/SKristMiner_jar/SKristMiner.jar -threads 8 -nonces 30000000 k640c183io
